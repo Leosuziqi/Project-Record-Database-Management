4034c12efad0bbdbd3331a1658d4f5f12273bb81
Leosuziqi <50681212+Leosuziqi@users.noreply.github.com>
2023-03-21 17:14:24 -0700

    Update search.py

--- search.py
+++ search.py
@@ -2,21 +2,22 @@
 # Data 19-Dec-2022
 
 import numpy as np
-import xlrd
 from tkinter import Tk, mainloop,TOP, ttk
 from openpyxl import Workbook
-#import cv2
 from tkinter.messagebox import showinfo
-# Data fetching from Excel files
 import pathlib
 
-#Search Function
+from TreeviewAction import *
+from fetch_calendar import *
+
+#Search if records match with contents from keyboard entry
 def search():
-    #search
     rows = []
     id_flag = FALSE
     Company_flag = FALSE
     Location_flag = FALSE
+    
+    #check which entry gets input
     if ProjectNum.get():
         search_id=ProjectNum.get()
         id_flag = TRUE
@@ -32,39 +33,37 @@ def search():
     ProjectNum.configure(state=tk.NORMAL)
     CompanyName.configure(state=tk.NORMAL)
     Location.configure(state=tk.NORMAL)
-    #email.configure(state=tk.NORMAL)
-
 
     #Save every matched row into cols, then append all cols to rows[] as the searched result
-    cols=[]
-    itr=2   #in case 
-    for cell in sheet.iter_rows(min_row=2,max_row=sheet.max_row,min_col=1,max_col=22,values_only=FALSE):    #Iteration in rows
-        #values_only?
+    cols=[] 
+    for itr, cell in sheet.iter_rows(min_row=2,max_row=sheet.max_row,min_col=1,max_col=22,values_only=FALSE):   
+        
+        # no input
         if not (id_flag|Company_flag|Location_flag):
             continue
+            
         #check if one or two or three inserted data matched
         if (id_flag and (str(search_id).lower() in str(sheet[itr][0].value).lower())) or id_flag == FALSE:
             #print(id_flag)
             #print(sheet[itr][0].value)
+            
+            #check matchs
             if (Company_flag and (str(search_Company).lower() in str(sheet[itr][1].value).lower())) or Company_flag == FALSE:
                 if (Location_flag and str(search_Location).lower() in str(sheet[itr][2].value).lower()) or Location_flag == FALSE:
+                    # save mathed result to ROWS
                     for index in cell:
-                        #print(index)
                         cols.append(index.value)
                     rows.append(cols)
                     cols=[]
         itr = itr + 1
 
     #print(len(rows))
-    display()
-
-    other_fucntions()
-
+    
+    # display result with Treeview API
     re_search=ttk.Button(result, text="Return to Search", command=back_to_search)
     re_search.place(relx=0.6,rely=0.855,width=100,height=40)
     exit_2 = ttk.Button(result, text="Close", command=search_close)
     exit_2.place(relx=0.8, rely=0.855, width=100, height=40)
-
 
     # Add Buttons to Edit and Delete the Treeview items
     treeview.bind('<Double-1>', selectItemtoEdit)
